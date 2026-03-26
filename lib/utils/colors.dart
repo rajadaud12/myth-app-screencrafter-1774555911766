f8a65ccc3780c81d5421ef57d5bbfe64305bb668
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-27 01:12:01 +0500

    Update lib/utils/colors.dart via MYTH Build System

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF00A8E8);
+  static const Color backgroundColor = Color(0xFFF5F9FC);
+  static const Color errorColor = Color(0xFFFF3B30);
+  static const Color successColor = Color(0xFF34C759);
+  static const Color pinkColor = Color(0xFFF4B7F5);
+  static const Color greenColor = Color(0xFF30D158);
+}
